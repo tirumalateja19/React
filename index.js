f7c222ea59ef89fe104e797ceb4a93651028b88a
tirumalateja19 <tirumalateja.jampani@gmail.com>
2025-01-12 08:13:14 +0530

    2nd episode

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "elem" }, "Welcome to React!!"),
-    React.createElement("h2", { id: "elem" }, "Welcome to React!!"),
+    React.createElement(
+      "h1",
+      { id: "elem" },
+      "Welcome to Namaste React 🚀🚀!!"
+    ),
+    React.createElement("h2", { id: "elem" }, "Welcome 🚀"),
   ])
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
